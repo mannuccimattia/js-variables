9e5f18207125ebb7b619654e269d3283ab80cac8
mannuccimattia <mannuccimattia@gmail.com>
2025-03-14 14:37:19 +0100

    [UPD] Done strings - 1

--- 02-strings/script.js
+++ 02-strings/script.js
@@ -1,6 +1,8 @@
 // 1. Dichiarazione di variabili
 // Crea una variabile chiamata name e assegnale il tuo nome.
+let myName = "Mattia";
 // Stampa il valore di nome nella console.
+console.log(myName);
 
 // 2. Tipi primitivi
 // Crea tre variabili chiamate number, text e bool e assegna rispettivamente
